f8ff3030eb3f9dbcde3d74da758cc415277b3fe8
DMN-SANGEETHD <dmnsangeeth@gmail.com>
2022-09-20 13:49:53 +0530

    add int and string

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ app = FastAPI()
 
 
 def index():
-    return {"Hello": "World"}
+    return {"data": "blog list"}
 
+
+
+
+@app.get('/blog/{id}')
+
+
+@app.get('/blog/unpublished')
+
+def unpublished():
+    return{'data':'all unpublished'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data':id}
 
