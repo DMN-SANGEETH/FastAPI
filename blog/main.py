a1b033afb1d91db178d5d052704a6a12477a41af
DMN-SANGEETH <dmnsangeeth@gmail.com>
2022-09-21 02:16:28 +0530

    added data to blog in data base

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,29 @@
 #from turtle import title
-from fastapi import FastAPI
+from turtle import title
+from fastapi import FastAPI,Depends
 from . import schemas, models
-from .database import engine
+from .database import SessionLocal, engine ,SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    
+    try:
+        yield db
+    finally:
+        db.close()
+        
 
 app = FastAPI()
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog =models.Blog(title=request.title, body=request.body)
+    
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    
+    return new_blog
