84710c3e00323c1cba79c305222d3156922fba7f
DMN-SANGEETH <dmnsangeeth@gmail.com>
2022-09-24 12:39:37 +0530

    step status show creater

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
 #from turtle import title
 from turtle import title
-from fastapi import FastAPI,Depends
+from urllib import response
+from fastapi import FastAPI,Depends, status, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine ,SessionLocal
 from sqlalchemy.orm import Session
@@ -18,7 +19,7 @@ def get_db():
 
 app = FastAPI()
 
-@app.post('/blog')
+@app.post('/blog', status_code = status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog =models.Blog(title=request.title, body=request.body)
     
@@ -33,7 +34,11 @@ def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}',status_code =200)
 def show(id,db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).filter(models.Blog.id==id).first()
+    
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with the id {id} is not avilable")
+        #response.status_code=status.HTTP_404_NOT_FOUND
     return blogs
