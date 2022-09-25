927d155538c9f03c356e6afb83eae168df4a6c58
DMN-SANGEETH <dmnsangeeth@gmail.com>
2022-09-25 06:58:09 +0530

    create to update blog

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,15 @@
 #from turtle import title
-from http.client import NO_CONTENT, NotConnected
+from email.policy import HTTP
+from http.client import ACCEPTED, NO_CONTENT, NotConnected
+from pyexpat import model
 from turtle import title
-from urllib import response
+from urllib import request, response
 from fastapi import FastAPI,Depends, status, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine ,SessionLocal
 from sqlalchemy.orm import Session
+
+import blog
 
 models.Base.metadata.create_all(engine)
 
@@ -32,10 +36,29 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def distroy(id,  db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blogs=db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    
+    if not blogs.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detial=f"Blog with with {id} not found")
+        
+    blogs.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request: schemas.Blog, db: Session = Depends(get_db) ):
+    blogs=db.query(models.Blog).filter(models.Blog.id==id)
+    
+    if not blogs.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detial=f"Blog with with {id} not found")
+    blogs.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'update'
+    
+    
+    
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
