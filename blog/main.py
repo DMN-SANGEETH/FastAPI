f4ad36c3bdff91ae6cac37baae77f8d8fe8df35b
DMN-SANGEETH <dmnsangeeth@gmail.com>
2022-09-28 12:46:29 +0530

    I creat respons

--- blog/main.py
+++ blog/main.py
@@ -64,7 +64,7 @@ def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}',status_code =200)
+@app.get('/blog/{id}',status_code =200,response_model=schemas.ShowBlog)
 def show(id,db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).filter(models.Blog.id==id).first()
     
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,12 @@
 
+from turtle import title
 from pydantic import BaseModel
 
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(BaseModel ):
+    title: str
+    class Config():
+        orm_model=True
